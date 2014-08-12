bde66f246ef9ea87e68b17a04e15ea03738fa86c
Hossein Zolfi <hossein.zolfi@gmail.com>
2014-08-13 00:21:26 +0430

    Q-1 implement check db template/route

--- Question-I/routes/migration_route.py
+++ Question-I/routes/migration_route.py
@@ -1,1 +1,13 @@
 __author__ = 'Hossein Zolfi <hossein.zolfi@gmail.com>'
+
+from flask import render_template
+from lib import app
+from lib.database import check_db
+
+@app.route('/check-db', methods=['GET'])
+def check_db():
+    from lib.database import check_db
+    if check_db():
+        return render_template('check_db/success.html')
+    return render_template('check_db/error.html')
+
