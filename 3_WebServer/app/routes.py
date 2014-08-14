9ffef19a906a339824cc6a7f48b7ad1cd825a50a
Hossein Zolfi <hossein.zolfi@gmail.com>
2014-08-14 21:21:10 +0430

    add my.aspx, time.php

--- 3_WebServer/app/routes.py
+++ 3_WebServer/app/routes.py
@@ -1,4 +1,5 @@
 import StringIO
+import datetime
 from http import router
 from http.routing.route import Route
 from server.server_requests_logger import ServerRequestLogger
@@ -45,9 +46,31 @@ def show_clients(request, response):
 
     return io.getvalue()
 
+def my_aspx(request, response):
+    io = StringIO.StringIO()
+
+    print >>io, '<h1>Request Headers</h1>'
+    print >>io, '<ul>'
+    for header_item in request.headers.iteritems():
+        print >>io, '<li>', ': '.join(header_item), '</li>'
+    print >>io, '<li>IP: %s</li>'%request['client_ip']
+    print >>io, '<li>PORT: %d</li>'%request['client_port']
+    print >>io, '</ul>'
+
+    return io.getvalue()
+
+def time_php(request, response):
+    io = StringIO.StringIO()
+
+    print >>io, 'Server time: ', datetime.datetime.now(), '<hr />'
+    print >>io, 'Host: ', request.headers['Host'].split(':')[0]
+    return io.getvalue()
+
 router.register_route(Route('GET', '/', hello_world))
 router.register_route(Route('GET', '/pic', show_pic))
 router.register_route(Route('GET', '/clients.html', show_clients))
+router.register_route(Route('GET', '/my.aspx', my_aspx))
+router.register_route(Route('GET', '/time.php', time_php))
 
 
 def bytes_from_file(filename):
